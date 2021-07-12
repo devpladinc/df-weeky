0d8e1261503edc547c0ce2f0e2df6357b2d7ebad
devpladinc <dev.pladinc@gmail.com>
2021-07-13 04:17:33 +0800

    Jul13 init:init
    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello world!"
+    
+@app.route('/about')
+def welcome():
+  return "Welcome to my Flask site, ©2018"
+   
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
+#   app.run(host='0.0.0.0', port=port)
