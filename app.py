db44aa0faa0ef62bc3707beabf7168e1d318d3db
devpladinc <dev.pladinc@gmail.com>
2021-07-13 04:34:35 +0800

    Jul13 fix: update port 2
    change port
    fix missing app file

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
     
-@app.route('/about')
-def welcome():
-  return "Welcome to my Flask site, ©2018"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+  return {
+        "fulfillmentText": 'This is from the heroku webhook',
+        "source": 'webhook'
+    }
    
 if __name__ == '__main__':
   # app.run(host='0.0.0.0', port=8080)
