6d97d713ce8fc4f7c45981dbf762a47d9de91dcd
devpladinc <dev.pladinc@gmail.com>
2021-07-13 04:25:20 +0800

    Jul13 fix: update port
    change port
    fix missing app file

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def welcome():
   return "Welcome to my Flask site, ©2018"
    
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
-#   app.run(host='0.0.0.0', port=port)
+  # app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=port)
