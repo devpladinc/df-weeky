deab5ee5072126a95102c874b42dda8c48965488
devpladinc <dev.pladinc@gmail.com>
2021-07-14 04:24:28 +0800

    Jul14 feat:add subtopic trigger
    add subtopic trigger

--- training_phrases.py
+++ training_phrases.py
@@ -24,3 +24,21 @@ topic_trigger = [
     'check some programming language',
     'help me with data science'
 ]
+
+ds_trigger = [
+    'I want to explore how to use azure in my projects',
+    'how to use AWS',
+    'how to get an AWS account',
+    'how to register with AWS',
+    'i need to use power bi on my projects',
+    'how to use power bi',
+    'what is sql',
+    'I want to study SQL',
+    'what does random Forest means and how use it',
+    'I need help in studying Logistics Regression',
+    'What is Logistics Regression',
+    'Explain how Linear Regression works',
+    'I want to learn K-means Clustering and how to use it',
+    'what is K-means Clustering',
+    'I want to explore how to use Decision Tree in my projects'
+]
